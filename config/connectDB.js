fd4179d3100ad1a249de22dce2de7e5104392907
samer-mansouri <74625806+samer-mansouri@users.noreply.github.com>
2022-03-28 19:57:56 +0200

    Updating database connection config

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 //2- connect to DB
 const connectDB = () => {
   mongoose
-    .connect('mongodb://localhost:27017/testmas')
-    .then(console.log("the database is connected..."))
-    .catch((err) => {});
+    .connect('mongodb://localhost:27017/testmas', { useNewUrlParser: true }, (err) => {
+      if (!err) { console.log('MongoDB Connection Succeeded.') }
+      else { console.log('Error in DB connection : ' + err) }
+  });
 };
 module.exports = connectDB;
